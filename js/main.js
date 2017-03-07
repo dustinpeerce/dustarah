d051dd38678254139c52ef56664759643b3c3b78
Dustin Peerce <dustin.a.peerce@gmail.com>
2017-03-07 04:30:15 -0500

    Final touches for going live

--- js/main.js
+++ js/main.js
@@ -13,14 +13,34 @@ $(document).ready(function(){
 
 	// Max height on Slider image and Map
 	$('#slider').css('max-height', $(window).height());
-	$('#map').css('max-height', $(window).height());
+	$('#map').css('max-height', $(window).height() - 70);
 
-	var sliderHeight = $('#slider').outerHeight(true) + 70;
+	// Position the slider text in the middle
+	var sliderHeight = $('#slider').innerHeight() + 70;
 	var sliderHeaderHeight = $('.slider-header-container').outerHeight(true);
 	var remainingSize = sliderHeight - sliderHeaderHeight;
 	var topValue = remainingSize/2;
 	$('.slider-header-container').css('top', topValue);
 
+	//jQuery to collapse the navbar on scroll
+	$(window).scroll(function() {
+	    if ($(".navbar").offset().top > 70) {
+	        $(".navbar").addClass("top-nav-collapse");
+	    } else {
+	        $(".navbar").removeClass("top-nav-collapse");
+	    }
+	});
+
+	//jQuery for page scrolling feature - requires jQuery Easing plugin
+	$(function() {
+	    $(document).on('click', 'a.page-scroll', function(event) {
+	        var $anchor = $(this);
+	        $('html, body').stop().animate({
+	            scrollTop: $($anchor.attr('href')).offset().top
+	        }, 1500, 'easeInOutExpo');
+	        event.preventDefault();
+	    });
+	});
 
 	$("#clients-logo").owlCarousel({
 		autoPlay: 3000,
@@ -40,15 +60,31 @@ $(document).ready(function(){
 	// google map
 	window.initMap = function() {
         var uluru = {lat: 36.995483, lng: -86.437271};
+
         var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 15,
           scrollwheel: false,
           center: uluru
         });
+
         var marker = new google.maps.Marker({
           position: uluru,
           map: map
         });
+
+        var contentString = '<p><strong>La Gala</strong><br>' +
+        					'601 State Street<br>' +
+        					'Bowling Green, KY 42101</p>';
+
+        var infowindow = new google.maps.InfoWindow({
+          content: contentString
+        });
+
+        marker.addListener('click', function() {
+          infowindow.open(map, marker);
+        });
+
+        infowindow.open(map, marker);
     }
 
 	// Counter
